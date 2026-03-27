6e7467d7f3d5aaa5c1c208cb75c497bd338f3d60
Rob8624 <robert_melen@yahoo.co.uk>
2026-03-27 14:44:56 +0000

    added strapi url to env

--- frontend/blog/src/lib/utils.ts
+++ frontend/blog/src/lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import clsx from 'clsx'
+import type { ClassValue } from 'clsx'
 
-export function cn(...inputs: ClassValue[]) {
+
+export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs))
+}
+
+
+export function getStrapiURL() {
+  return import.meta.env.VITE_STRAPI_URL || "http://localhost:1337";
 }
