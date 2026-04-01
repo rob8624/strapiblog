50337c3517f8d5dd6320e37e31867c34bb7e6b1b
Rob8624 <robert_melen@yahoo.co.uk>
2026-04-01 21:37:34 +0100

    header redesign

--- frontend/blog/src/components/header.tsx
+++ frontend/blog/src/components/header.tsx
@@ -14,26 +14,29 @@ export function Header({ header }: HeaderProps) {
  
   return (
      
-    <div className="flex flex-col sm:flex-row items-center flex-wrap">
-      <img className='w-20 sm:w-40 pl-2 sm:pl-0' src={`${strapiURL}${header.logo.url}`} />
-      <div className='flex flex-1 justify-start gap-3 sm:ml-[10%]'>
-        <div className='flex flex-col'>
-          <div className='font-head leading-tight tracking-tight text-2xl sm:text-4xl 
-          text-center sm:text-left border-t-2 border-b-2 pt-2 pb-3  '>
-              <span className='text-primary-foreground'>Welcome. Just a </span>
-              <span className='inline-block -rotate-3 p-1 
-              shadow-sm bg-primary text-accent-foreground 
-              text-2xl sm:text-6xl'>Blog, </span>
-              <span> about </span>
-              <span className='inline-block -rotate-3 p-1 
-              shadow-sm bg-primary text-accent-foreground 
-              text-2xl sm:text-6xl m-2 sm:pt-0'> ...things </span>
-          </div>
-          <nav>
-            <Menu items={header.link} />
-          </nav>
-         </div>
-      </div>
+    
+      
+      
+    <div className='flex flex-col'>
+  <div className='flex items-center   py-2 gap-4'>
+    
+    {/* logo */}
+    <img className='w-40 sm:w-60 shrink-0' src={`${strapiURL}${header.logo.url}`} />
+    
+    {/* text */}
+    <div className='font-head leading-tight tracking-tight text-2xl sm:text-4xl flex flex-wrap'>
+      <span className='text-primary-foreground'>Just a </span>
+      <span className='inline-block -rotate-3 p-1 sm:relative static ml-2 mr-2 bottom-6 shadow-sm bg-primary text-accent-foreground text-2xl sm:text-6xl'>Blog</span>
+      <span> about, </span>
+      <span className='inline-block -rotate-3 p-1 shadow-sm bg-primary text-accent-foreground text-2xl sm:text-6xl m-2'> ...things </span>
     </div>
+
+  </div>
+  <nav>
+    <Menu items={header.link} />
+  </nav>
+</div>
+      
+   
   )
 }
