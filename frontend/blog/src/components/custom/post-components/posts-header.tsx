6627cf644cd78cf2056c1440fe1aac938fd31864
rob8624 <robert_melen@yahoo.co.uk>
2026-06-01 12:57:07 +0100

    fixed author section mobile view

--- frontend/blog/src/components/custom/post-components/posts-header.tsx
+++ frontend/blog/src/components/custom/post-components/posts-header.tsx
@@ -13,10 +13,10 @@ export function PostHeader({ title, excerpt, publishedAt, author }: PostHeaderPr
     <header className="flex flex-col justify-center items-center text-center">
       <h1 className="font-head text-2xl sm:text-3xl pt-5">{title}</h1>
       <p className="text-sm">{excerpt}</p>
-      <div className="flex gap-2 pt-5 items-center">
+      <div className="flex flex-col sm:flex-row sm:gap-2 pt-5 items-center">
         <div className="text-md">Published on {formatDate(publishedAt)}</div>
-        <div>by {author?.name}</div>
-        <img src={author?.avatar?.formats?.thumbnail?.url ?? author?.avatar?.url}  alt={title} className="w-7 rounded-full"/>
+        <div>by <span className="font-bold">{author?.name}</span></div>
+        <img src={author?.avatar?.formats?.thumbnail?.url ?? author?.avatar?.url}  alt={title} className="w-7 rounded-full pb-2 sm:pb-0"/>
       </div>
     </header>
   )
